c91239452b47c9db5e0211562d4ebd9fc092873f
Halukx <00halukozdemir@gmail.com>
2022-05-25 17:00:06 +0300

    Save Load edited

--- 1.LEVEL/Level1/Assets/Script/MainMenu.cs
+++ 1.LEVEL/Level1/Assets/Script/MainMenu.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject playbutton;
+    [SerializeField] GameObject controlsButton;
+    [SerializeField] GameObject optionButton;
+    [SerializeField] GameObject quitButton;
+    [SerializeField] GameObject newGame;
+    [SerializeField] GameObject resumeGame;
+    private void Start()
+    {
+        playbutton.SetActive(true);
+        controlsButton.SetActive(true);
+        optionButton.SetActive(true);
+        quitButton.SetActive(true);
+        newGame.SetActive(false);
+        resumeGame.SetActive(false);
+    }
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        playbutton.SetActive(false);
+        controlsButton.SetActive(false);
+        optionButton.SetActive(false);
+        quitButton.SetActive(false);
+        newGame.SetActive(true);
+        resumeGame.SetActive(true);
     }
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadGame()
+    {
+
     }
 }
